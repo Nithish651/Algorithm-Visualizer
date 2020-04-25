ab97d73fd7ab59e2f37d79828d190b3cd918ade6
Nithish <nithish651@gmail.com>
2020-04-25 14:49:14 +0530

    bubble sort implemented

--- scripts/sorting.js
+++ scripts/sorting.js
@@ -1,9 +1,13 @@
+let dataset = null;
+
+
 function generateDataSet(){
     console.log("inside");
     let board = document.getElementById("board");
     board.innerHTML = '';
     let random  = Array.from({length:100}, () => Math.floor(Math.random() *450) );
-    console.log(random);
+    dataset = random;
+
     
     for(i=0; i< random.length; i++){
         let element = document.createElement("div");
@@ -11,4 +15,41 @@ function generateDataSet(){
         element.style.height = random[i];
         board.appendChild(element);
     }
+}
+
+async function bubbleSort(){
+    
+    let speed = document.getElementById("speed").value;
+    console.log(speed);
+    let board = document.getElementById("board");
+    let data = board.children;
+    let length = dataset.length;
+    
+    for(i=0; i< length-1; i++){
+        for(j=0; j < length - 1 - i; j++){
+            
+             data[j].classList.add('picked');
+             data[j+1].classList.add('picked');
+             await sleep(speed);
+           
+            if (dataset[j] > dataset[j+1]){
+
+                    let temp = dataset[j+1];
+                    dataset[j+1] = dataset[j];
+                    dataset[j] = temp;
+                    board.insertBefore(data[j+1],data[j]);
+                    await sleep(speed);
+
+            }
+        
+            data[j].classList.remove('picked');
+            data[j+1].classList.remove('picked');
+        }
+    }
+                
+                console.log(dataset);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
